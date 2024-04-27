477d30bf14b39b4763016fd1f29aeee9ac2033bf
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-04-27 12:32:41 +0600

    carousel slider bug fix

--- src/Components/HomePage/MyCarouselSlider.js
+++ src/Components/HomePage/MyCarouselSlider.js
@@ -2,58 +2,32 @@ import React from 'react';
 import slide1 from "../../assets/slide1.webp"
 import slide2 from "../../assets/slide2.webp"
 import slide3 from "../../assets/slide3.webp"
-import { Link } from 'react-router-dom';
 
 
 const MyCarouselSlider = () => {
     return (
-        
-      <div className="carousel w-full h-3/4">
+      <div className="carousel w-full h-4/5">
       <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src={slide1}
-          className="w-full"
-          alt="image1"
-        />
+        <img alt='image1' src={slide1} className="w-full"/>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <Link to="" href="#slide3" className="btn btn-circle">
-            ❮
-          </Link>
-          <Link to="" href="#slide2" className="btn btn-circle">
-            ❯
-          </Link>
+          <a href="#slide3" className="btn btn-circle">❮</a> 
+          <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
-      </div>
+      </div> 
       <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src={slide2}
-          className="w-full"
-          alt="image2"
-        />
+        <img alt='image2' src={slide2} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <Link to="" href="#slide1" className="btn btn-circle">
-            ❮
-          </Link>
-          <Link to="" href="#slide3" className="btn btn-circle">
-            ❯
-          </Link>
+          <a href="#slide1" className="btn btn-circle">❮</a> 
+          <a href="#slide3" className="btn btn-circle">❯</a>
         </div>
-      </div>
+      </div> 
       <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src={slide3}
-          className="w-full"
-          alt="image3"
-        />
+        <img alt='image3' src={slide3} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <Link to="" href="#slide2" className="btn btn-circle">
-            ❮
-          </Link>
-          <Link to="" href="#slide1" className="btn btn-circle">
-            ❯
-          </Link>
+          <a href="#slide2" className="btn btn-circle">❮</a> 
+          <a href="#slide1" className="btn btn-circle">❯</a>
         </div>
-      </div>
+      </div> 
     </div>
     );
 };
