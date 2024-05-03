8ffb7a7696d0359e6a4b3fe5ff05b524a1d74f6d
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-05-03 12:27:56 +0600

    drawer setup done

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <Outlet></Outlet>
+          
         </div>
-    );
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 };
 export default Layout;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import {
   MdLocationOn,
 } from "react-icons/md";
 import { IoPersonCircleOutline, IoSearch } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
@@ -13,13 +13,8 @@ const Navbar = () => {
     <div className="shadow-lg">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle"
-            >
-              <svg
+          <label htmlFor="my-drawer" >
+          <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
                 fill="none"
@@ -33,22 +28,8 @@ const Navbar = () => {
                   d="M4 6h16M4 12h16M4 18h7"
                 />
               </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {/* <li>
-                <Link to="">Homepage</a>
-              </li>
-              <li>
-                <Link to="">Portfolio</a>
-              </li>
-              <li>
-                <Link to="">About</a>
-              </li> */}
-            </ul>
-          </div>
+          </label>
+
           <button className="btn btn-ghost btn-circle">
             <Link to="favorite">
               <MdLocationOn className="h-5 w-5" />
