267a7a4b8b658b9ccf7050e3152a7c28d4122828
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-05-07 23:29:16 +0600

    checkout start

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const CheckOut = () => {
-    return (
-        <div className=' min-h-screen'>
-            
+  return (
+    <div className=" min-h-screen">
+      <div>
+        <div>
+          <label className="input input-bordered flex items-center gap-2">
+            Name
+            <input type="text" className="grow" placeholder="Daisy" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            Email
+            <input type="text" className="grow" placeholder="daisy@site.com" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <kbd className="kbd kbd-sm">⌘</kbd>
+            <kbd className="kbd kbd-sm">K</kbd>
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <span className="badge badge-info">Optional</span>
+          </label>
         </div>
-    );
+
+
+        
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
